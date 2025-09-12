1227bea4f3a0d0917bb5e5e7bfaf4e10714eca98
imcrisam <Imcrisam@gmail.com>
2025-09-12 23:06:44 +0000

    bug terminal devcontainer

--- .devcontainer/terminal_zsh.sh
+++ .devcontainer/terminal_zsh.sh
@@ -9,4 +9,6 @@ apt-get install -y zsh git curl
 RUNZSH=no CHSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 # Cambiar shell por defecto
-chsh -s $(which zsh) root
+chsh -s "$(which zsh)" root
+
+exec zsh -l
