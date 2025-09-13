c3d40809ef6e7b9503bc81944df6614600f39360
imcrisam <Imcrisam@gmail.com>
2025-09-13 05:33:44 +0000

    service, import module

--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Vehicle } from './entities/vehicle.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class VehiclesService {
+
+   constructor(
+    @InjectRepository(Vehicle)
+    private readonly vehicleRepo: Repository<Vehicle>,
+  ) {}
+
   create(createVehicleDto: CreateVehicleDto) {
-    return 'This action adds a new vehicle';
+    const vehicle = this.vehicleRepo.create(createVehicleDto);
+    return this.vehicleRepo.save(vehicle);
   }
 
   findAll() {
-    return `This action returns all vehicles`;
+    return this.vehicleRepo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} vehicle`;
+  async findOne(id: string) {
+    const vehicle = await this.vehicleRepo.findOne({ where: { id } });
+    if (!vehicle) throw new NotFoundException(`Vehicle ${id} not found`);
+    return vehicle;
   }
 
   update(id: number, updateVehicleDto: UpdateVehicleDto) {
